2d4a672ff894ff94f4f841c4768f6d2e7ab8959d
do00ob <1955788420@qq.com>
2023-09-07 20:33:59 +0800

    12

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="350px">Aside</el-aside>
+        <el-main><myTable msg="ee"/></el-main>
+      </el-container>
+    </el-container>
+  </div>
+
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <!--li v-for="(data,index) in tableData" :key="index">{{data}}</li-->
+  </ul>
+  
+  
+</template>
+
+<script >
+import HelloWorld from './components/HelloWorld.vue'
+import myTable from './components/Table.vue';
+
+
+
+const tableRowClassName = ({
+  row,
+  rowIndex,
+}) => {
+  rowIndex = tableData.width
+  if (rowIndex === 1) {
+    return 'warning-row'
+  } else if (rowIndex === 3) {
+    return 'success-row'
+  }
+  return ''
+}
+
+
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    myTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.el-table .warning-row {
+  --el-table-tr-bg-color: var(--el-color-warning-light-9);
+}
+.el-table .success-row {
+  --el-table-tr-bg-color: var(--el-color-success-light-9);
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="id" label="id" width="180" />
+    <el-table-column prop="name" label="name" width="180" />
+    <el-table-column prop="gender" label="gender" />
+  </el-table>
+
+  <el-row class="mb-4">
+    <el-button type="success" round>增加数据</el-button>
+    <el-button type="danger" round>删除数据</el-button>
+    <el-button type="primary" round>查找数据</el-button>
+    <el-button type="warning" round>改动数据</el-button>
+  </el-row>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    created: function () {
+        axios.get("http://localhost:8088/user/get").then((response) => {
+            this.tableData = response.data
+        })
+    },
+    data() {
+        return {
+            tableData: []
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
